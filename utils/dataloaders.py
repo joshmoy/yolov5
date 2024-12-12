150080a6141d1dcfe7b143b10c9287602851c2a5
CodeChef <55160504+joshmoy@users.noreply.github.com>
2024-12-12 00:08:51 +0000

    Update dataloaders.py
    update image folder route for VOC2012 data
    
    Signed-off-by: CodeChef <55160504+joshmoy@users.noreply.github.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -531,7 +531,7 @@ def img2label_paths(img_paths):
     """Generates label file paths from corresponding image file paths by replacing `/images/` with `/labels/` and
     extension with `.txt`.
     """
-    sa, sb = f"{os.sep}images{os.sep}", f"{os.sep}labels{os.sep}"  # /images/, /labels/ substrings
+    sa, sb = f"{os.sep}JPEGImages{os.sep}", f"{os.sep}labels{os.sep}"  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit(".", 1)[0] + ".txt" for x in img_paths]
 
 
